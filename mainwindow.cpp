ed46614ce510daeacf0db260c30f8b9e4dc0558f
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-05-09 16:27:49 +0300

    Add GPL license notices

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright 2023 Grigoriy Efimov
+ *
+ * Author: Grigoriy Efimov <efimov-gm@newpage.xyz>
+ *
+ * This file is part of grille-crypt-qt.
+ *
+ * Grille-crypt-qt is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Grille-crypt-qt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with grille-crypt-qt.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
