a2a2625649ef8872a97e6b7ef53efb7eb03de5f7
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-05-09 21:23:42 +0300

    Add desktop file to arch package

--- build/archlinux/build.sh
+++ build/archlinux/build.sh
@@ -5,8 +5,11 @@ cmake --build .
 makepkg
 
 rm grille-crypt-qt
+rm grille-crypt -r
+rm grille-crypt-qt_autogen -r
 rm pkg -r
-rm src -r
+rm src/grille-crypt-qt
 rm Makefile
 rm CMa* -r
 rm cmake_install.cmake
+rm .qt -r
