68d2d59cb39004df7b6832d211acaf29a432c1cf
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-05-09 20:26:49 +0300

    Include grille-crypt as submodule

--- actionwindow.cpp
+++ actionwindow.cpp
@@ -1,12 +1,34 @@
+/*
+ * Copyright 2023 Grigoriy Efimov
+ *
+ * Author: Grigoriy Efimov <efimov-gm@newpage.xyz>
+ *
+ * This file is part of grille-crypt-qt.
+ *
+ * Grille-crypt-qt is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * Grille-crypt-qt is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with grille-crypt-qt.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "actionwindow.h"
 #include <QString>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QLineEdit>
+#include <iostream>
 
 ActionWindow::ActionWindow(QWidget *parent,ActionTypes at)
     : QWidget{parent}
 {
-
+    this->at = at;
     if(at == ActionTypes::encrypt){
         buttonAction = new QPushButton("Ecnrypt");
     }
@@ -19,8 +41,12 @@ ActionWindow::ActionWindow(QWidget *parent,ActionTypes at)
 
     inputLabel = new QLabel();
     outputLabel = new QLabel();
+    passcodeLabel = new QLabel();
     inputLabel->setText("Input File");
     outputLabel->setText("Output File");
+    passcodeLabel->setText("Password");
+    password = new QLineEdit();
+    password->setEchoMode(QLineEdit::Password);
 
     box = new QHBoxLayout();
 
@@ -28,6 +54,9 @@ ActionWindow::ActionWindow(QWidget *parent,ActionTypes at)
     box->addWidget(buttonInputFile);
     box->addWidget(outputLabel);
     box->addWidget(buttonOutputFile);
+    box->addWidget(passcodeLabel);
+    box->addWidget(password);
+    box->addWidget(passcodeLabel);
     box->addWidget(buttonAction);
 
     setLayout(box);
@@ -39,17 +68,30 @@ ActionWindow::ActionWindow(QWidget *parent,ActionTypes at)
 }
 
 void ActionWindow::doAction(){
-    if( (inputPath == "") & (outputPath=="") ){
-        QMessageBox::warning(this, "Warning", "Fill input filename and output filename",QMessageBox::Close);
+    QString passCode = password->text();
+    if( (inputPath == "") || (outputPath=="") || (passCode=="") ){
+        QMessageBox::warning(this, "Warning", "Fill input filename, output filename and password",QMessageBox::Close);
         return;
     }
+    grille::GrilleCrypt gc{};
+
+
+    buttonAction->setEnabled(false);
+    if(this->at == ActionTypes::encrypt){
+        gc.Encrypt(inputPath.toStdString(), outputPath.toStdString(), passCode.toStdString());
+    }
+    else{
+        gc.Decrypt(inputPath.toStdString(), outputPath.toStdString(), passCode.toStdString());
+    }
+    QMessageBox msgBox;
+    msgBox.setText("Done");
+    msgBox.exec();
+
     inputPath = "";
     outputPath = "";
     buttonInputFile->setText("...");
     buttonOutputFile->setText("...");
-    QMessageBox msgBox;
-    msgBox.setText("Done");
-    msgBox.exec();
+    buttonAction->setEnabled(true);
 }
 
 void ActionWindow::getInputFileName()
@@ -65,5 +107,5 @@ void ActionWindow::getOutputFileName()
     QString path = QFileDialog::getSaveFileName(this,tr("Find Files"), QDir::currentPath());
     QString outputFileText = "..." + path.mid(path.size()-5,5);
     buttonOutputFile->setText(outputFileText);
-    inputPath = path;
+    outputPath = path;
 }
